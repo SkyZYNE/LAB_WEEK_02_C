9685bfab74ccdf981168036ecb66a0f037d6b183
SkyZYNE <raffaelgunawan19@gmail.com>
2025-09-06 21:11:41 +0700

    Commit No. 4 – Standard activity and single top activity

--- app/src/main/java/com/example/lab_week_02_c/StandardActivity.kt
+++ app/src/main/java/com/example/lab_week_02_c/StandardActivity.kt
@@ -3,44 +3,31 @@ package com.example.lab_week_02_c
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class StandardActivity : AppCompatActivity() {
     companion object {
         private const val DEBUG = "DEBUG"
     }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_standard)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
         Log.d(DEBUG, "onCreate")
-        val buttonClickListener = View.OnClickListener { view ->
-            when (view.id) {
-                R.id.button_standard -> startActivity(
-                    Intent(
-                        this,
-                        StandardActivity::class.java
-                    )
-                )
-
-                R.id.button_single_top -> startActivity(
-                    Intent(
-                        this,
-                        SingleTopActivity::class.java
-                    )
-                )
-            }
-
+        findViewById<Button>(R.id.button_standard).setOnClickListener{
+            startActivity(
+                Intent(this,
+                    StandardActivity::class.java)
+            )
         }
     }
 }
